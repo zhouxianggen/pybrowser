657fbb321c5071427916701bc6ebefda587bd08f
zhouxianggen <zhouxianggen@gmail.com>
2019-07-20 23:02:25 +0800

    update

--- pybrowser/pybrowser.py
+++ pybrowser/pybrowser.py
@@ -1,119 +1,54 @@
 # coding: utf8 
 """ 提供模拟浏览器环境 
 """
+from __future__ import absolute_import, unicode_literals
 import signal
-import time
-import asyncio
-import pyua
 import psutil
 import pyppeteer
 from pyobject import PyObject
 
 
-class BrowserContext(PyObject):
+class PyBrowser(PyObject):
     def __init__(self, proxy_server=None):
         PyObject.__init__(self)
-        self.name = ''
-        self.browser = None
-        self.connected = None
         self.proxy_server = proxy_server
-        self.deadline = 0
+        self.browser = None
+        self.pid = None
 
 
-    async def recheck(self):
-        pages = await self.browser.pages()
-        if len(pages) == 1 and pages[0].url == 'about:blank':
-            self.deadline = 0
+    async def __aenter__(self):
+        await self.launch()
+        return self.browser
+
+    
+    async def __aexit__(self, exc_type, exc, tb):
+        self.log.info('close browser')
+        await self.browser.close()
+        self.force_close()
 
 
-    async def get_browser(self):
-        if not self.browser or not self.connected:
-            await self.reset()
-            return None
-        if self.deadline:
-            if self.deadline < time.time():
-                await self.reset()
-            return None
-        pages = await self.browser.pages()
-        self.log.info('[{}] current pages'.format(self.name))
-        for idx,p in enumerate(pages):
-            self.log.info('\t[{}:{}:{}]'.format(idx, p.isClosed(), p.url))
-        if len(pages) == 1 and pages[0].url == 'about:blank':
-            return self.browser
-        await self.reset()
-        return None
-
-
-    async def reset(self):
-        self.log.info('reset')
-        if self.browser:
-            self.suicide()
-            #await self.close_browser()
-        while not self.connected:
-            await self.launch_browser()
-        
-
-    async def launch_browser(self):
+    async def launch(self):
         self.log.info('launch browser')
         args = ['--no-sandbox']
         if self.proxy_server:
             args.append('--proxy-server={}'.format(self.proxy_server))
-        self.browser = await pyppeteer.launch(args=args, 
+        self.browser = await pyppeteer.launch(args=args,
                 ignoreHTTPSErrors=True, handleSIGHUP=False, autoClose=False)
-        self.log.info('browser launched on [{}][{}]'.format(
+        self.pid = self.browser.process.pid
+        self.log.info('browser launched [{}][{}]'.format(
                 self.browser.process.pid, self.browser.wsEndpoint))
-        self.name = str(self.browser.process.pid)
-        self.connected = True
-        self.deadline = 0
-
-    
-    def on_disconnected(self):
-        self.log.warning('Disconnected')
-        self.connected = False
 
 
-    async def close_browser(self):
-        self.log.info('close current browser [{}][{}]'.format(
-                self.browser.process.pid, self.browser.wsEndpoint))
-        await self.browser.close()
-        self.name = ''
-        self.browser = None
-        self.connected = False
-        self.deadline = 0
-
-
-    def suicide(self):
-        sig = signal.SIGTERM
-        pid = self.browser.process.pid
-        self.log.info('suicide [{}]'.format(pid))
+    def force_close(self):
         try:
-            parent = psutil.Process(pid)
-            children = parent.children(recursive=True)
-            for c in children:
-                c.send_signal(sig)
-            parent.send_signal(sig)
+            parent = psutil.Process(self.pid)
+            # todo: check if is a chrome process
         except psutil.NoSuchProcess:
-            pass
-        self.name = ''
-        self.browser = None
-        self.connected = False
-        self.deadline = 0
+            return
+        self.log.info('force close browser')
+        sig = signal.SIGTERM
+        children = parent.children(recursive=True)
+        for c in children:
+            c.send_signal(sig)
+        parent.send_signal(sig)
 
-
-class BrowserPool(PyObject):
-    def __init__(self, pool_size=5, proxy_server=None):
-        PyObject.__init__(self)
-        self.pool = [BrowserContext(proxy_server) for i in range(pool_size)]
-
-
-    async def get_browser_context(self, deadline=5):
-        self.log.info('get browser context')
-        for ctx in self.pool:
-            self.log.info('check [{}]'.format(ctx.name))
-            browser = await ctx.get_browser()
-            if browser is not None:
-                self.log.info('get [{}]'.format(ctx.name))
-                ctx.daedline = deadline
-                return ctx
-        return None
-    
--- pybrowser/test.py
+++ pybrowser/test.py
@@ -1,37 +1,20 @@
 # coding: utf8
+import sys
 import asyncio
-import json
 import pyua
-from pyutil import get_logger
-from pybrowser import BrowserPool
+from pybrowser import PyBrowser
 
 
-log = get_logger()
-url = 'https://weixin.sogou.com/weixin?type=2&s_from=input&query=%E9%82%B5%E4%B8%9C&ie=utf8&_sug_=n&_sug_type_='
-pool = BrowserPool()
-
+url = 'https://blog.csdn.net/xidianbaby/article/details/87790422'
 
 async def foo():
-    while True:
-        c = await pool.get_browser_context()
-        if c is not None:
-            break
-    page = await c.browser.newPage()
-    await page.setUserAgent(pyua.CHROME)
-    await page.goto(url)
-    await page.screenshot({'path': '/data/share/foo.png'})
-    await page.waitForXPath('//*[@id="pagebar_container"]/div')
-    es = await page.Jx('//*[starts-with(@id, "sogou_vr_11002601_title_")]')
-    hrefs = []
-    for e in es:
-        href = await page.evaluate("e => e.href", e)
-        log.info('click [{}]'.format(href))
-        await e.click()
-        await asyncio.sleep(1)
-        pages = await c.browser.pages()
-        for idx,p in enumerate(pages):
-            log.info('\t[{}:{}:{}]'.format(idx, p.isClosed(), p.url))
-        await asyncio.sleep(2)
-
+    async with PyBrowser() as b:
+        print('open url')
+        page = await b.newPage()
+        await page.setUserAgent(pyua.CHROME)
+        await page.goto(url, timeout=5000)
+        await page.screenshot({'path': '/data/share/foo.png'})
+        content = await page.content()
+        open('content', 'w').write(content)
 
 asyncio.run(foo())
